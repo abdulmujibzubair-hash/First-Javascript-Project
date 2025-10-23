9e2ac98541775d4c3c27db7cff8a1bdd61ccc7eb
abdulmujibzubair-hash <abdulmujibzubair@gmail.com>
2025-10-23 12:17:31 +0100

    created the layout

---
+++ test.js
@@ -0,0 +1,43 @@
+alert("Hello, World!");
+alert("Welcome To Zubayr Tech Hub");
+
+var myAge = 29;
+
+if  (myAge > 30){
+
+    document.write("you are over 30");
+
+} else if (myAge > 20){
+
+    document.write("you are over 20");
+
+} else {
+
+    document.write("you are not over 10");
+}
+
+
+var myAge = 25;
+
+if (myAge >= 18 && myAge <= 30) {
+
+    document.write ("You are eligible for this programme");
+
+} else {
+
+    document.write("you are not in compliance with the requirement of this program");
+
+}
+
+
+var myAge = 5;
+
+while (myAge < 10) {
+
+    console.log("your age is less than 10");
+    myAge++;
+
+}
+
+
+document.write("you are over 10");
